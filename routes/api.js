22d0f61774e547f47ccd364a3a42faee4cb71559
LynnGe <nzgezilin@gmail.com>
2016-04-08 09:10:15 +1200

    change json to jsonp

--- routes/api.js
+++ routes/api.js
@@ -15,13 +15,33 @@ router.get('/data', function(req, res, next) {
   connection.connect();
 
   let sqls = [
-    `SELECT * FROM lynn.menu`,
-    `SELECT * FROM lynn.info`
+    `SELECT * FROM infos`,
+    `SELECT * FROM menus`,
+    `SELECT * FROM languages`,
+    `SELECT * FROM menus`,
+    `SELECT * FROM portfolios`,
+    `SELECT * FROM portfolios p where p.title in ('mekongBaby','jstea')`,
+    `SELECT * FROM faqs`
   ].join(";");
+  let result = {};
+  let facebook = {};
   connection.query(sqls, function(err,rows) {
     if (!err)
-      console.log(rows);
-      res.json(rows);
+
+    facebook.id = rows[3].length+1;
+    facebook.url = rows[0][0].url;
+    facebook.display = rows[0][0].display;
+
+    result.infos = rows[0][0];
+    result.menus = rows[1];
+    result.languages = rows[2];
+    result.footerMenus = rows[3].concat(facebook);
+    result.portfolios = rows[4];
+    result.services = rows[5];
+    result.faqs = rows[6];
+
+    console.log(result);
+    res.jsonp(result);
   });
   connection.end();
 });
